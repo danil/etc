11c47c901da60fc67d3dd845c133c012d20cbe88
Danil Kutkevich <danil@kutkevich.org>
2012-03-09 23:40:52 +0400

    leela-gentoo initial commit

---
+++ acpi/actions/lid.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Danil.
+# The script uses the hibernate-ram command from
+# sys-power/hibernate-script - make sure the package is installed on
+# your system.
+
+if [ -x "/usr/sbin/hibernate-ram" ]
+then
+    /usr/sbin/hibernate-ram
+else
+    logger "${0}: hibernate-ram command does not exist. Aborting."
+    exit 1
+fi
